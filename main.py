0989e73b316f87cdac2696f50d79c92cba44c190
hamani31 <ewetanh@students.dsbn.org>
2025-03-24 12:34:49 -0400

    average

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-
+#Name: Hamani ewetan
+#Date: 24/3/25
+#Title: List and average
+#discription: this program makes a list of a hundred random numbers and then finds the average of those hundred numbers
+import random
+numbers=[]
+for index in range(1,100):   
+    num=random.randint(0,100)
+    print(num)
+    numbers.append(num)
+average=(sum(numbers)/100)
+print("average is", average)
+        
